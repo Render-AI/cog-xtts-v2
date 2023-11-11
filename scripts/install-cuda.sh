a964ee6fa40801a48833699e54d3d893fb3561d2
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-11-11 00:51:59 -0800

    cuda 11.9

--- scripts/install-cuda.sh
+++ scripts/install-cuda.sh
@@ -50,11 +50,11 @@ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/
 sudo apt update && sudo apt upgrade -y
 
  # installing CUDA-11.8
-sudo apt install cuda-12-3 -y
+sudo apt install cuda-11-8 -y
 
 # setup your paths
-echo 'export PATH=/usr/local/cuda-12.3/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-12.3/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PATH=/usr/local/cuda-11.8/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
 sudo ldconfig
 
@@ -62,14 +62,14 @@ sudo ldconfig
 # First register here: https://developer.nvidia.com/developer-program/signup
 
 CUDNN_TAR_FILE="cudnn-linux-x86_64-8.7.0.84_cuda11-archive.tar.xz"
-sudo wget https://developer.download.nvidia.com/compute/redist/cudnn/v8.7.0/local_installers/12.3/cudnn-linux-x86_64-8.7.0.84_cuda11-archive.tar.xz
+sudo wget https://developer.download.nvidia.com/compute/redist/cudnn/v8.7.0/local_installers/11.8/cudnn-linux-x86_64-8.7.0.84_cuda11-archive.tar.xz
 sudo tar -xvf ${CUDNN_TAR_FILE}
 sudo mv cudnn-linux-x86_64-8.7.0.84_cuda11-archive cuda
 
 # copy the following files into the cuda toolkit directory.
-sudo cp -P cuda/include/cudnn.h /usr/local/cuda-12.3/include
-sudo cp -P cuda/lib/libcudnn* /usr/local/cuda-12.3/lib64/
-sudo chmod a+r /usr/local/cuda-12.3/lib64/libcudnn*
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-11.8/include
+sudo cp -P cuda/lib/libcudnn* /usr/local/cuda-11.8/lib64/
+sudo chmod a+r /usr/local/cuda-11.8/lib64/libcudnn*
 
 # Finally, to verify the installation, check
 nvidia-smi
