7014d71035fa10d3e5e3ff4056a9801d643389b3
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-11-11 21:16:49 -0800

    init

--- predict.py
+++ predict.py
@@ -1,13 +1,18 @@
 # Prediction interface for Cog
 from cog import BasePredictor, Input, Path
 import os
+import torch
 from TTS.api import TTS
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         os.environ["COQUI_TOS_AGREED"] = "1"
-        self.model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to('cuda')
+        self.model = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
+        if torch.cuda.is_available():
+            self.model = self.model.to('cuda')
+        else:
+            print("CUDA is not available. Model will run on CPU.")
 
     def predict(
         self,
