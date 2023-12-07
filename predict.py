67774601fd1bc821fbfd642fcf42d61e44b8414b
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-07 14:43:20 -0800

    sync

--- predict.py
+++ predict.py
@@ -15,19 +15,32 @@ class Predictor(BasePredictor):
             description="Text to synthesize",
             default="Hi there, I'm your new voice clone. Try your best to upload quality audio"
         ),
-        speaker_wav: Path = Input(description="Original speaker audio"),
+        speaker: Path = Input(description="Original speaker audio (wav, mp3, m4a, ogg, or flv)"),
         language: str = Input(
-            description="Language",
-            choices=["en", "es", "fr", "de", "it", "pt", "pl", "tr", "ru", "nl", "cs", "ar", "zh-cn"],
+            description="Output language for the synthesised speech",
+            choices=["en", "es", "fr", "de", "it", "pt", "pl", "tr", "ru", "nl", "cs", "ar", "zh", "hu", "ko", "hi"],
             default="en"
+        ),
+        cleanup_voice: bool = Input(
+            description="Whether to apply denoising to the speaker audio (microphone recordings)",
+            default=False
         ),
     ) -> Path:
         """Run a single prediction on the model"""
+        speaker_wav = "/tmp/speaker.wav"
+        filter = "highpass=75,lowpass=8000,"
+        trim_silence = "areverse,silenceremove=start_periods=1:start_silence=0:start_threshold=0.02,areverse,silenceremove=start_periods=1:start_silence=0:start_threshold=0.02"
+        # ffmpeg convert to wav and apply afftn denoise filter. y to overwrite and avoid caching
+        if cleanup_voice:
+            os.system(f"ffmpeg -i {speaker} -af {filter}{trim_silence} -y {speaker_wav}")
+        else:
+            os.system(f"ffmpeg -i {speaker} -y {speaker_wav}")
+
         path = self.model.tts_to_file(
             text=text, 
-            file_path = "output.wav",
+            file_path = "/tmp/output.wav",
             speaker_wav = speaker_wav,
-            language= language
+            language = language
         )
 
         return Path(path)
