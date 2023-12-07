aedb4745119986a97cedd16c3a8c880e63f4793b
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-07 15:14:41 -0800

    scripts

--- scripts/install-nvidia-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# update nvidia-docker
-sudo apt install nvidia-docker2
---
+++ scripts/redeploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# git clone
+sudo rm -rf cog-xtts-v2
+git clone https://github.com/render-ai/cog-xtts-v2
+bash cog-xtts-v2/scripts/deploy.sh
