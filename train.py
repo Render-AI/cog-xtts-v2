21cbbd5f22c6528a6911003b81157863f0d8b015
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-16 22:22:01 -0800

    Update train.py

--- train.py
+++ train.py
@@ -12,10 +12,10 @@ import librosa
 import numpy as np
 
 
-from TTS.demos.xtts_ft_demo.utils.formatter import format_audio_list
-from TTS.demos.xtts_ft_demo.utils.gpt_train import train_gpt
-from TTS.tts.configs.xtts_config import XttsConfig
-from TTS.tts.models.xtts import Xtts
+from tts.TTS.demos.xtts_ft_demo.utils.formatter import format_audio_list
+from tts.TTS.demos.xtts_ft_demo.utils.gpt_train import train_gpt
+from tts.TTS.tts.configs.xtts_config import XttsConfig
+from tts.TTS.tts.models.xtts import Xtts
 
 
 # from TTS.demos.xtts_ft_demo.utils.formatter import format_audio_list
