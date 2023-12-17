049fcb8717cbb3c3176a98e186e2bdc69e5148e5
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-16 20:23:19 -0800

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ class TrainingOutput(BaseModel):
     weights: Path
 
 def train(
-    prefix: str = Input(description="data you wish to save"),
+    prefix: str = Input(description="data you wish to save", default="Test"),
 ) -> TrainingOutput:
     weights = Path("output.txt")
     with open(weights, "w") as f:
