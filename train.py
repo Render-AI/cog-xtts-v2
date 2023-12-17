c5321302637542ce9d8f8fe6fb677eba59ddc0c9
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-16 20:28:48 -0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,20 @@
-from cog import BaseModel, Input, Path
+from cog import BaseModel, Input, Path, File
+import logging
+import io
+import argparse
+import os
+import sys
+import tempfile
+import librosa.display
+import numpy as np
+import os
+import torch
+import torchaudio
+import traceback
+from TTS.demos.xtts_ft_demo.utils.formatter import format_audio_list
+from TTS.demos.xtts_ft_demo.utils.gpt_train import train_gpt
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
 
 class TrainingOutput(BaseModel):
     weights: Path
